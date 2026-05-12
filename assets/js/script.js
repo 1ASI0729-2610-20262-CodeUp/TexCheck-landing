fdd54de5dd927247062b1849cb36ff8882cf32b6
Sebastián Díaz <97618144+tipaso07@users.noreply.github.com>
2026-05-11 23:19:38 -0500

    Feat: Add i18n initialization and locale button handlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+import { initI18n, setLocale } from "../i18n/i18n.js";
 
+await initI18n();
+
+document.querySelectorAll("[data-locale-btn]").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    setLocale(btn.getAttribute("data-locale-btn"));
+  });
+});
